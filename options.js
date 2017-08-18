282853012408743c7c65c01dc1dc8e67de581127
Nicholas Vadivelu <nick@bassoft.com>
2017-08-17 22:17:55 -0400

    added block sites section to options

--- options.js
+++ options.js
@@ -1,7 +1,7 @@
-var defaultColor = "blue";
-
 function blockSites() {
-    
+    var text = document.getElementById('myTextArea').value; 
+    console.log('test');
+    console.log(text);
 }
 
 function retrain() { //sets the previous retrain date early enough that extension retrains
