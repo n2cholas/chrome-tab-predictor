3e661e1643a8f80adf82ecd2e077b5f6dc2bffeb
Nicholas Vadivelu <nick@bassoft.com>
2017-08-17 23:14:07 -0400

    added setBlockedSites to options

--- options.js
+++ options.js
@@ -1,7 +1,14 @@
-function blockSites() {
+var StorageArea = chrome.storage.local;
+
+function blockSites() { //adds list of blocked sites to storage area
     var text = document.getElementById('myTextArea').value; 
-    console.log('test');
-    console.log(text);
+    StorageArea.set({'blockedSites': text});
+}
+
+function setBlockedSitesArea() { //returns list of blocked sites
+    StorageArea.get('blockedSites', function(blockedSites){
+        document.getElementById("myTextarea").value = blockedSites;
+    })
 }
 
 function retrain() { //sets the previous retrain date early enough that extension retrains
