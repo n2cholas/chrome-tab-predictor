e218150453e47e898d4582063ce63e47dacc9e89
Lawrence Pang <lawrencepang36@gmail.com>
2017-08-20 22:52:47 -0400

    get buttons to execute script

--- options.js
+++ options.js
@@ -1,28 +1,33 @@
-var StorageArea = chrome.storage.local;
-
 function blockSites() { //adds list of blocked sites to storage area
-    alert("Block Sites activated")
+	var StorageArea = chrome.storage.local;
+    console.log("Block Sites activated")
     var text = document.getElementById('myTextArea').value;
     StorageArea.set({'blockedSites': text});
 }
 
 function retrain() { //sets the previous retrain date early enough that extension retrains
-    alert("Retrain will commence when you close this tab.");
+	var StorageArea = chrome.storage.local;
+    console.log("Retrain will commence when you close this tab.");
     StorageArea.get('retrainTime', function(retrainTime ) {
 	    StorageArea.set({ 'date': Date.now()-retrainTime-1 }, function () {});
     });
 }
 
-StorageArea.get('blockedSites', function(blockedSites){
-    if (blockedSites == null || blockedSites == "[object Object]") {
-        alert("test")
-        StorageArea.set({ 'blockedSites': "" }, function () {
-        });
-    } else {
-        alert(blockedSites);
-        document.getElementById("myTextArea").value = blockedSites;
-    }
-});
+function load() {
+	var StorageArea = chrome.storage.local;
+	console.log("test")
+	StorageArea.get('blockedSites', function(blockedSites){
+		if (blockedSites == null || blockedSites == "[object Object]") {
+			alert("test")
+			StorageArea.set({ 'blockedSites': "" }, function () {
+			});
+		} else {
+			alert(blockedSites);
+			document.getElementById("myTextArea").value = blockedSites;
+		}
+	});
+	document.getElementById("blockSitesBtn").addEventListener("click", blockSites);
+	document.getElementById("retrainBtn").addEventListener("click", retrain);
+}
 
-document.getElementById("blockSitesBtn").addEventListener("click", blockSites);
-document.getElementById("retrainBtn").addEventListener("click", retrain);
+window.onload = load;
